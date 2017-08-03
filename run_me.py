70cad9c292b228ed39f1957bd2cf0411b5b67ed5
minhcoding000 <minhcoding000@gmail.com>
2017-08-03 10:28:04 -0400

    1.1

--- run_me.py
+++ run_me.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import*
+from pygame.locals import *
 import random
 import time
 
@@ -11,7 +11,7 @@ pygame.font.init()
 pygame.init()
 
 #Game Configuration
-fps = 40                 #Speed of worm
+fps = 30                 #Speed of worm
 width = 1000
 height = 800
 score = 0
@@ -20,18 +20,20 @@ snakeColor = (250,250,250)
 snake2Color = (140,10,200)
 wallColor = (0,250,0)
 foodColor = (250,0,0)
-screen = pygame.display.set_mode((width,height),pygame.RESIZABLE)
+playerNames = []
+players = 0
+
 #width = pygame.display.Info().current_w
 #height = pygame.display.Info().current_h
 
 #Some sprite configuration
-foodInc = 50            #Increase snake's length for foodInc pixel long
+foodInc = 20            #Increase snake's length for foodInc pixel long
 foodx = foody = 10      #Food X-Size
 snakex = snakey = 5     #Size of one part of Snake
 iniLength = 10          #Initial Snake Length
 vel = 5                 #Velocity must be equal to snake pixel size or will cause pixel-collision problem
-snake1Pos = [width * 10/100,height * 90/100]
-snake2Pos = [width * 90/100,height * 90/100]
+snake1Pos = [width * 90/100,height * 90/100]
+snake2Pos = [width * 10/100,height * 90/100]
 
 #Font Configuration
 BASICFONT = pygame.font.Font('freesansbold.ttf',18)
@@ -53,13 +55,13 @@ YELLOW = (255,255,0)
 BLACK = (0,0,0)
 
 #Load Sound
-song_loc = '/home/pi/mostec17/audio/'
-sounds = ['wallCrash','self_crash','food_touch','snake_crash','di de','genghis_khan']
-pygame.mixer.init()
+#song_loc = '/home/pi/mostec17/audio/'
+#sounds = ['wallCrash','self_crash','food_touch','snake_crash','di de','genghis_khan']
+#pygame.mixer.init()
 #pygame.mixer.music.load(song_loc+sounds[]+'.mp3')
 #pygame.mixer.music.play()
-selfCrash = pygame.mixer.Sound('self_crash.wav')
-selfCrash.play()
+#selfCrash = pygame.mixer.Sound('self_crash.wav')
+#selfCrash.play()
 #foodTouch = pygame.mixer.Sound(song_loc+sounds[2]+'.wav')
 #snakeCrash = pygame.mixer.Sound(song_loc+sounds[3]+'.wav')
 
@@ -78,7 +80,7 @@ class Food:
     def position(self):
         return self.x,self.y
 
-    def check(self,x,y):
+    def check(self, x, y):
         if x < self.x or x > self.x + foodx:
             return False
         elif y < self.y or y > self.y + foody:
@@ -132,6 +134,7 @@ class Worm:
             self.crashed = True
         elif (r,g,b) == snake2Color:
             self.crashed = True
+            
         #if (self.x,self.y) in self.body:   
          #   self.crashed = True
         
@@ -161,6 +164,7 @@ class Worm:
 
     def position(self):
         return self.x, self.y
+    
 #----------------------------------------------------------------------------
 class Worm2:
     # A second worm
@@ -230,23 +234,101 @@ class Worm2:
 
     def position(self):
         return self.x, self.y
+
 #----------------------------------------------------------------------------
 
 
 def main():
-    result = gameRun()
-    if result == 1:
-        pass
-    if result == 2:
-        pass
-    if result == 3:
-        pass
+    global players
+    players = getPlayers()
+    getNames(players)
+    while True:
+        if players == "1":
+            result = gameRun()
+        elif players == "2":
+            result = gameRun2()
+        if result == 1:
+            terminate()
+            while True:
+                qac = input("Restart? (y/n))")
+                if qac == "y" or qac == "n":
+                    break
+                else:
+                    print("Invalid. Please try again")
+                    continue
+            if qac == "y":
+                print("Restarting...")                
+                continue
+            elif qac == "n":
+                print("Ending game...")
+                break
+        if result == 2:
+            terminate()
+            while True:
+                qac = input("Restart? (y/n))")
+                if qac == "y" or qac == "n":
+                    break
+                else:
+                    print("Invalid. Please try again")
+                    continue
+            if qac == "y":
+                print("Restarting...")                
+                continue
+            elif qac == "n":
+                print("Ending game...")
+                break           
+    terminate()
     
         
-#Initialize 
+#Initialize
 def gameRun():
     global screen
     global score
+    screen = pygame.display.set_mode((width,height),pygame.RESIZABLE)
+    w = Worm(screen)
+    f = Food(screen)
+    screen.fill(bgColor)
+    pygame.draw.rect(screen,wallColor,(0,0,width,10),0)
+    pygame.draw.rect(screen,wallColor,(0,0,10,height),0)
+    pygame.draw.rect(screen,wallColor,(width-10,0,10,height),0)
+    pygame.draw.rect(screen,wallColor,(0,height-10,width,10),0)
+    running = True
+
+    while running:
+        w.move()
+        w.draw(False)    #Draw before move because we need that box to be colored first before collision can be detected
+        f.draw()
+
+        #Collision Detection for Snake 1
+        if w.crashed:
+            running = False
+            print("Snake 1 Collides")
+        elif f.check(w.x,w.y):       #Send snake's head position
+            score += 1
+            w.eat()
+            print("Score: %d" %score)
+            f.erase()
+            w.draw(True)
+            f = Food(screen)          # make new food
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                w.key_event(event)
+                    
+        pygame.display.flip()
+        clock.tick(fps)
+    time.sleep(3)
+    return 1    
+
+
+def gameRun2():
+    global amount
+    global screen
+    global score
+    screen = pygame.display.set_mode((width,height),pygame.RESIZABLE)
     w = Worm(screen)
     w2 = Worm2(screen)
     f = Food(screen)
@@ -291,15 +373,54 @@ def gameRun():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                terminate()
             elif event.type == pygame.KEYDOWN:
                 w.key_event(event)
                 w2.key_event(event)
                     
         pygame.display.flip()
         clock.tick(fps)
-
+    time.sleep(3)
+    return 2
 def terminate():
     pygame.display.quit()
     pygame.quit()
 
+def getPlayers():
+    passed = False
+    while passed == False:
+        number = input("Number of Players(1-2): ")
+        if number == "1":
+            break
+        if number == "2":
+            break
+        else:
+            print("Not Valid")
+            continue
+    print(number)
+    return number
+
+def getNames(players):
+    global playerNames
+    if players == "1":
+        i = input("Your name: ")
+        playerNames.append(i)
+    elif players == "2":
+        i = input("Name of First Player: ")
+        print("First Player's Name: {}".format(i))
+        playerNames.append(i)
+        i2 = input ("Name of Second Player: ")
+        print("Second Player's Name: {}".format(i))
+        playerNames.append(i2)
+    return
+
+
 main()
+
+
+
+
+
+
+
+
